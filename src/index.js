0e06313863da2490ff20c01f7ebbe45aaf7356a8
Bartłomiej <bartlomiej.ostojski@teb.edu.pl>
2023-02-13 16:57:58 +0100

    Builded project structure

---
+++ src/index.js
@@ -0,0 +1,13 @@
+require("dotenv").config({path: "env/.env"});
+const server = require("./server")
+const port = process.env.PORT;
+
+console.log(process.env.PORT)
+
+const startServer = () => {
+  server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  })
+}
+
+startServer();
