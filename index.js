af5d7f8df0b1bef8def4b8626172354db3d88139
Kr3t2115 <szymon.baranowicz@teb.edu.pl>
2023-02-13 15:50:14 +0100

    change

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Worl dsadd!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello Wor witam witam!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
