f7bdf865a0299762736318763ab2686e34cdce6c
Bartłomiej <bartlomiej.ostojski@teb.edu.pl>
2023-10-09 21:38:53 +0200

    Updated documentation

--- src/server.js
+++ src/server.js
@@ -5,7 +5,8 @@ const bodyParser = require('express').json;
 const cors = require('cors');
 const routes = require("./routes");
 const cookies = require("cookie-parser");
-const expressJSDocSwagger = require('express-jsdoc-swagger');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocumentOne = require('./swagger-output.json');
 
 app.use(
     cors({ 
@@ -15,47 +16,12 @@ app.use(
     })
 );
 
-const options = {
-    info: {
-      version: '1.0.0',
-      title: 'trycrypto API',
-      license: {
-        name: 'MIT',
-      },
-    },
-    security: {
-      BasicAuth: {
-        type: 'http',
-        scheme: 'basic',
-      },
-    },
-    // Base directory which we use to locate your JSDOC files
-    baseDir: __dirname,
-    // Glob pattern to find your jsdoc files (multiple patterns can be added in an array)
-    filesPattern: './**/*.js',
-    // URL where SwaggerUI will be rendered
-    swaggerUIPath: '/api-docs',
-    // Expose OpenAPI UI
-    exposeSwaggerUI: true,
-    // Expose Open API JSON Docs documentation in `apiDocsPath` path.
-    exposeApiDocs: false,
-    // Open API JSON Docs endpoint.
-    apiDocsPath: '/v1/api-docs',
-    // Set non-required fields as nullable by default
-    notRequiredAsNullable: false,
-    // You can customize your UI options.
-    // you can extend swagger-ui-express config. You can checkout an example of this
-    // in the `example/configuration/swaggerOptions.js`
-    swaggerUiOptions: {},
-    // multiple option in case you want more that one instance
-    multiple: true,
-  };
-  
-  expressJSDocSwagger(app)(options);
+var options = {}
+
+app.use('/api-docs', swaggerUi.serveFiles(swaggerDocumentOne, options), swaggerUi.setup(swaggerDocumentOne));
 
 app.use(cookies());
 app.use(bodyParser());
 app.use(routes);
-
 
 module.exports = app;
